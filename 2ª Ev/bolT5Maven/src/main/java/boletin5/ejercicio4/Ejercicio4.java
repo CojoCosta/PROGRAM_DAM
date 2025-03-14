9ccb0cc3f3960467e9433aa63cb9361a1eae2b57
CojoCosta <dcosta1882000@gmail.com>
2025-03-14 12:21:14 +0100

    changes

--- 2ª Ev/bolT5Maven/src/main/java/boletin5/ejercicio4/Ejercicio4.java
+++ 2ª Ev/bolT5Maven/src/main/java/boletin5/ejercicio4/Ejercicio4.java
@@ -1,3 +1,28 @@
+/*-
+ * =====LICENSE-START=====
+ * Java 11 Application
+ * ------
+ * Copyright (C) 2020 - 2025 Organization Name
+ * ------
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * =====LICENSE-END=====
+ */
 package boletin5.ejercicio4;
 
 public class Ejercicio4 {
@@ -19,8 +44,7 @@ public class Ejercicio4 {
      * posición
      * 
      * @param frase              cadena principal
-     * @param posicionInicio     posición de la cadena donde empezamos a devolver
-     *                           caracteres
+     * @param posicionInicio     posición de la cadena donde empezamos a devolver caracteres
      * @param cantidadCaracteres nº de caracteres que devolvemos
      * @return La subcadena especificada
      */
@@ -40,7 +64,6 @@ public class Ejercicio4 {
     // #region C)
     /**
      * Función que muestra una cadena centrada en un nargen de 80 caracteres
-     * 
      * @param frase cadena que pasamos
      */
     public static void muestraCentrado(String frase) {
--- 2ª Ev/bolT5Maven/src/main/java/boletin5/ejercicio8/Coleccion.java
+++ 2ª Ev/bolT5Maven/src/main/java/boletin5/ejercicio8/Coleccion.java
@@ -1,3 +1,28 @@
+/*-
+ * =====LICENSE-START=====
+ * Java 11 Application
+ * ------
+ * Copyright (C) 2020 - 2025 Organization Name
+ * ------
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * =====LICENSE-END=====
+ */
 package boletin5.ejercicio8;
 
 import java.io.File;
@@ -17,10 +42,14 @@ public class Coleccion {
     }
 
     public ArrayList<Videojuego> getVideojuegos() {
-
         return videojuegos;
     }
-
+    
+    /**
+     * Función que mira la longitud de un atring y devuelve el setring tal cual si mide menos de 23 caracteres o lo acorta a 20 y añade puntos suspesivos
+     * @param cadenaFormateada  String que se pasa 
+     * @return String de 23 caracteres de longitud como maxima
+     */
     public static String formatearString(String cadenaFormateada) {
         if (cadenaFormateada.length() > 23) {
             return cadenaFormateada.substring(0, 19) + "...";
@@ -28,7 +57,12 @@ public class Coleccion {
             return cadenaFormateada;
         }
     }
-
+    
+    /**
+     * Función que busca en una lista elementos Videojuego mediante un string que es el comienzo de éste
+     * @param cadenaFormateada string para buscar elementos de la lista
+     * @return lista elementos Videojuego de la lista Videojuego que comienza su nombre por el string que se pasa como parametro
+     */
     public ArrayList<Videojuego> comienzoDeJuego(String letrasComienzo) {
         ArrayList<Videojuego> juegosEncontrados = new ArrayList<>();
         for (Videojuego videojuego : getVideojuegos()) {
@@ -40,7 +74,9 @@ public class Coleccion {
         }
         return juegosEncontrados;
     }
-
+    /**
+     * Funcion que contiene un menu con todas las opciones para modificar la lista de elementos Videojuego
+     */
     public void menu() throws Exception {
         Scanner sc = new Scanner(System.in);
         Videojuego nuevoVideojuego = new Videojuego(2020, "Diego");
@@ -127,10 +163,8 @@ public class Coleccion {
                 File file = new File("videojuegos.txt");
                 PrintWriter write = new PrintWriter(file);
                 if (file.exists()) {
-                    
+                    write.println(videojuegos);
                 }
-
-
                     break;
             }
 
