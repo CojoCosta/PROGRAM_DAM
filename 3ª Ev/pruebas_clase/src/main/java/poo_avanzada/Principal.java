75239799f75199241db778d714920919d551d12b
CojoCosta <dcosta1882000@gmail.com>
2025-03-20 13:40:34 +0100

    changes

--- 3ª Ev/pruebas_clase/src/main/java/poo_avanzada/Principal.java
+++ 3ª Ev/pruebas_clase/src/main/java/poo_avanzada/Principal.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Principal {
     public static void main(String[] args) {
-//-------Clase Animal y subclases-------//
+        // -------Clase Animal y subclases-------//
         Mosca m = new Mosca();
         Perro p = new Perro(5, "Palleiro", "Lucky");
         System.out.println(m.nombreCientifico);
@@ -26,30 +26,40 @@ public class Principal {
         System.out.println(p.nombreCientifico);
         ((Perro) objAnimal).ladrar();
 
-//-ARRAY DE TIPO ANIMAL QUE PUEDE CONTENER TODAS SUS SUBCLASES-//
+        // -ARRAY DE TIPO ANIMAL QUE PUEDE CONTENER TODAS SUS SUBCLASES-//
         Animal[] bichos = new Animal[3];
         bichos[0] = new Perro(5, "Pastor", "Laika");
         bichos[1] = new Mosca();
         bichos[2] = new Mosca();
-        for(Animal animal : bichos){
+        for (Animal animal : bichos) {
             if (animal.getClass() == Perro.class) {
-                System.out.println(((Perro)animal).nombreHumano);
-            } else{
-                if(animal.getClass() == Mosca.class){
+                System.out.println(((Perro) animal).nombreHumano);
+            } else {
+                if (animal.getClass() == Mosca.class) {
                     animal.muestraDatos();
-                } else{
+                } else {
                     System.out.println(animal.nombreCientifico);
                 }
             }
             System.out.println(animal.nombreCientifico);
         }
 
-//---ArrayList de 'Object' que puede meter de todo---//
+        // ---ArrayList de 'Object' que puede meter de todo---//
         ArrayList<Object> cosas = new ArrayList<>();
         cosas.add("hola");
         cosas.add(4);
         cosas.add(new Perro());
         cosas.add(bichos);
 
+// ----------------EQUALS USADO POR 'contains' ------------------//
+        ArrayList<Perro> perros = new ArrayList<>();
+        perros.add(new Perro(2, "Pastor", "Richard"));
+        perros.add(new Perro(5, "Palleiro", "Lucky"));
+        perros.add(new Perro(5, "Pekines", "Buff"));
+        perros.add(new Perro(5, "Caniche", "Thor"));
+
+        Perro p1 = new Perro(5, "Pekines", "Buff");
+
+        System.out.printf("%s %s existe en la colección\n", p.nombreHumano, perros.contains(p) ? "Sí" : "No");
     }
 }
