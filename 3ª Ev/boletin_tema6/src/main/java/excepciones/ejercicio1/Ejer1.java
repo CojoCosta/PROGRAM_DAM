4a922679517b2333d2483a0ef30022ecebba1950
CojoCosta <dcosta1882000@gmail.com>
2025-03-21 11:45:10 +0100

    changes

--- 3ª Ev/boletin_tema6/src/main/java/excepciones/ejercicio1/Ejer1.java
+++ 3ª Ev/boletin_tema6/src/main/java/excepciones/ejercicio1/Ejer1.java
@@ -1,53 +1,63 @@
 package excepciones.ejercicio1;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Ejer1 {
         public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n;
         double euro;
+        int n;
         do {
-            System.out.println("\n1. Introduce la cantidad de euros que quieres pasar a libras.");
-            System.out.println("2. Introduce la cantidad de libras que quieres pasar a euros.");
-            System.out.println("3. Salir.");
-            System.out.println();
-            n = sc.nextInt();
-            switch (n) {
-                case 1: // € a libra 1*0.83
-
-                    System.out.print("Cuantos euros quieres pasar a libras: "); // Comprobación de valor positivo
-                    euro = sc.nextDouble();
-                    while (euro < 0) {
-                        System.out.println("Debes introducir un valor positivo");
+            try {
+                System.out.println("\n1. Introduce la cantidad de euros que quieres pasar a libras.");
+                System.out.println("2. Introduce la cantidad de libras que quieres pasar a euros.");
+                System.out.println("3. Salir.");
+                n = sc.nextInt();
+                switch (n) {
+                    case 1: // € a libra 1*0.83
+                    try {
                         System.out.print("Cuantos euros quieres pasar a libras: "); // Comprobación de valor positivo
                         euro = sc.nextDouble();
+                        while (euro < 0) {
+                            System.out.println("Debes introducir un valor positivo");
+                            System.out.print("Cuantos euros quieres pasar a libras: "); // Comprobación de valor positivo
+                            euro = sc.nextDouble();
+                        }
+                        euro = euro * 0.83;
+                        System.out.printf("Son %.2f libras", euro);
+                    } catch (InputMismatchException e) {
+                        System.out.println("Introduce un valor númerico");
                     }
-                    euro = euro * 0.83;
-                    System.out.printf("Son %.2f libras", euro);
-
+                    
                     break;
-                case 2: // libra a € 1/0.83
-                    System.out.print("Cuantas libras quieres pasar a euros: "); // Comprobación de valor positivo
-                    euro = sc.nextDouble();
-                    while (euro < 0) {
-                        System.out.println("Debes introducir un valor positivo");
+                    case 2: // libra a € 1/0.83
+                    
                         System.out.print("Cuantas libras quieres pasar a euros: "); // Comprobación de valor positivo
                         euro = sc.nextDouble();
-                    }
-                    euro = euro / 0.83;
-                    System.out.printf("Son %.2f euros", euro);
-
+                        while (euro < 0) {
+                            System.out.println("Debes introducir un valor positivo");
+                            System.out.print("Cuantas libras quieres pasar a euros: "); // Comprobación de valor positivo
+                            euro = sc.nextDouble();
+                        }
+                        euro = euro / 0.83;
+                        System.out.printf("Son %.2f euros", euro);
+                    
                     break;
-                case 3:
+                    case 3:
                     System.out.println("Gracias por venir ");
-
+                    
                     break;
-
-                default:
+                    
+                    default:
                     break;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Introduce un valor númerico");
+                sc.nextLine();
+                n = 0;
             }
-
-        } while (n != 3);
+                
+            } while (n != 3);
+        }
     }
-}
