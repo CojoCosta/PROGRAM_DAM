09859fb024c2a211b29ce79faee37763fe4ba390
CojoCosta <dcosta1882000@gmail.com>
2025-04-03 13:54:12 +0200

    changes

--- 3ª Ev/boletin_tema6_b/src/main/java/excepciones_pooavanzada/ejercicio1/Ejercicio1.java
+++ 3ª Ev/boletin_tema6_b/src/main/java/excepciones_pooavanzada/ejercicio1/Ejercicio1.java
@@ -6,19 +6,15 @@ import java.util.Scanner;
 public class Ejercicio1 {
     // #region B)
     /**
-     * Función que devuelve una cantidad de caracteres desde una determinada
-     * posición
-     * 
+     * Función que devuelve una cantidad de caracteres desde una determinada posición
      * @param frase              cadena principal
-     * @param posicionInicio     posición de la cadena donde empezamos a devolver
-     *                           caracteres
+     * @param posicionInicio     posición de la cadena donde empezamos a devolver caracteres
      * @param cantidadCaracteres nº de caracteres que devolvemos
      * @return La subcadena especificada
      */
     public static String subCadena(String frase, int posicionInicio, int cantidadCaracteres) {
         String cadenaFinal = "";
-        if (posicionInicio < 0 || posicionInicio > frase.length()
-                || (posicionInicio + cantidadCaracteres) > frase.length()) {
+        if (posicionInicio < 0 || posicionInicio > frase.length() || (posicionInicio + cantidadCaracteres) > frase.length()) {
             throw new IllegalArgumentException("Has hecho algo mal");
         } else {
             for (int i = posicionInicio; i < cantidadCaracteres + posicionInicio; i++) {
@@ -32,15 +28,14 @@ public class Ejercicio1 {
     // #region C)
     /**
      * Función que muestra una cadena centrada en un nargen de 80 caracteres
-     * 
      * @param frase cadena que pasamos
      */
     public static void muestraCentrado(String frase) {
-        if (frase.length() > 80) {
-            throw new InputMismatchException("El String es demasiado largo, se pasa de 80 caracteres");
-        }
-        int mitadDeAncho = 80 / 2;
-        int largoDeCadena = frase.length() / 2;
+            if (frase.length() > 80) {
+                throw new InputMismatchException("El String es demasiado largo, se pasa de 80 caracteres");
+            }
+        int mitadDeAncho = 80/2;
+        int largoDeCadena = frase.length()/2;
         int centrado = mitadDeAncho - largoDeCadena;
         String espacios = " ";
         for (int i = 0; i < centrado; i++) {
@@ -48,7 +43,6 @@ public class Ejercicio1 {
         }
         System.out.println(frase);
     }
-
     // #endregion
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
--- 3ª Ev/boletin_tema6_b/src/main/java/excepciones_pooavanzada/ejercicio2/geomteria/Linea.java
+++ 3ª Ev/boletin_tema6_b/src/main/java/excepciones_pooavanzada/ejercicio2/geomteria/Linea.java
@@ -1,5 +1,36 @@
 package excepciones_pooavanzada.ejercicio2.geomteria;
 
+import excepciones_pooavanzada.ejercicio2.interfaz.Libreria;
+
 public class Linea extends Figura{
+    private Punto puntoFinal;
+    
+    public void setPuntoFinal(Punto puntoFinal) {
+        this.puntoFinal = puntoFinal;
+    }
+    public Punto getPuntoFinal() {
+        return puntoFinal;
+    }
+
+    @Override
+    public void pedirDatos() {
+        super.pedirDatos();
+        puntoFinal = new Punto(Libreria.pedirReal(), Libreria.pedirReal());
+    }
+
+    @Override
+    public void mostrarDatos() {
+        super.mostrarDatos();
+        System.out.println(puntoFinal);
+    }
+
+    public Linea(Punto puntoOrigen, Punto puntoFinal){
+        super(puntoOrigen, "linea");
+        setPuntoFinal(puntoFinal);
+    }
+
+    public Linea(){
+        this(new Punto(0, 0), new Punto(1, 1));
+    }
     
 }
