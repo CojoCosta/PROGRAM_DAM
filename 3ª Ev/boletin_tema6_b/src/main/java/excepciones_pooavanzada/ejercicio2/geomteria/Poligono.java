b02aeca8ead8c772872f45d1669919cd4e4e8738
CojoCosta <dcosta1882000@gmail.com>
2025-04-04 09:53:30 +0200

    changes

--- 3ª Ev/boletin_tema6_b/src/main/java/excepciones_pooavanzada/ejercicio2/geomteria/Poligono.java
+++ 3ª Ev/boletin_tema6_b/src/main/java/excepciones_pooavanzada/ejercicio2/geomteria/Poligono.java
@@ -3,10 +3,16 @@ package excepciones_pooavanzada.ejercicio2.geomteria;
 public class Poligono extends Figura{
     public Punto[] puntos;
 
-    public Poligono(String nombre, Punto origen, Punto[] puntos, int tamañoDelArray){
+    //TODO TODO MAL LLAMAR A CURRO
+    public Poligono(String nombre, Punto origen, int puntos, int tamañoDelArray){
+        setNombre("triangulo");
+        setOrigen(new Punto(0, 0));
+        for (int i = 0; i < this.puntos.length; i++) {
+            puntos = (int)(Math.random());
+        }
         
     }
     public Poligono(){
-        
+        this(nombre, origen, 0, 3);
     }
 }
