f46eda9d567a1bdb99465e1213ce305846fb9c0d
CojoCosta <dcosta1882000@gmail.com>
2025-04-07 10:41:03 +0200

    changes

--- 3ª Ev/pruebas_clase/src/main/java/poo_avanzada/GUI_SWING/FrmPrincipal.java
+++ 3ª Ev/pruebas_clase/src/main/java/poo_avanzada/GUI_SWING/FrmPrincipal.java
@@ -1,19 +1,46 @@
 package poo_avanzada.GUI_SWING;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-class FrmPrincipal extends JFrame{
+class FrmPrincipal extends JFrame {
 
     // Inicializamos la interfaz de usuario en el constructor
     public FrmPrincipal() {
 
-        // Llamando a super podemos inicializar el título de la ventana
-        super("The Sultans of Swing");
-        // Creamos una “Etiqueta de texto” y especificamos su toolTip
-        JLabel label = new JLabel("La aplicación perfecta");
-        label.setToolTipText("Esto es un componente JLabel");
-        // Incluímos la JLabel en la colección de componentes de la ventana
-        this.add(label);
+        // // Llamando a super podemos inicializar el título de la ventana
+        // super("The Sultans of Swing");
+        // // Creamos una “Etiqueta de texto” y especificamos su toolTip
+        // JLabel label = new JLabel("La aplicación perfecta");
+        // label.setToolTipText("Esto es un componente JLabel");
+        // // Incluímos la JLabel en la colección de componentes de la ventana
+        // this.add(label);
+
+        setLayout(new FlowLayout());
+        JLabel etiqueta1 = new JLabel("Etiqueta Uno");
+        JLabel etiqueta2 = new JLabel("Etiqueta Dos");
+        JButton boton1 = new JButton("Aceptar");
+        JButton boton2 = new JButton("Cancelar");
+
+        etiqueta1.setLocation(0, 0);
+        etiqueta2.setLocation(100, 0);
+        boton1.setLocation(0, 100);
+        boton2.setLocation(100, 100);
+
+        //.add sin colocar se sobre escriben uno encima del otro y solo se ve el ultimo
+        // this.add(etiqueta1);
+        // this.add(etiqueta2);
+        // this.add(boton1);
+        // this.add(boton2);
+
+        //.add con distribución para que se vean todos
+        this.add(etiqueta1, BorderLayout.NORTH);
+        this.add(etiqueta2, BorderLayout.SOUTH);
+        this.add(boton1, BorderLayout.EAST);
+        this.add(boton2, BorderLayout.WEST);
     }
 }
