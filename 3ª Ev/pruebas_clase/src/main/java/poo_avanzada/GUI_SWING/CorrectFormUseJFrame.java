c799a8d71ab723938ed0e93a4ee1fe2870dbebb7
CojoCosta <dcosta1882000@gmail.com>
2025-04-10 12:56:47 +0200

    changes

--- 3ª Ev/pruebas_clase/src/main/java/poo_avanzada/GUI_SWING/CorrectFormUseJFrame.java
+++ 3ª Ev/pruebas_clase/src/main/java/poo_avanzada/GUI_SWING/CorrectFormUseJFrame.java
@@ -8,6 +8,12 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 class FrmPrincipal extends JFrame {
+    //Declaramos siempre asi
+    private JLabel etiquetas;
+    JLabel etiqueta;
+    private JButton botones;
+    JButton boton;
+
 
     // Inicializamos la interfaz de usuario en el constructor
     public FrmPrincipal() {
@@ -20,24 +26,32 @@ class FrmPrincipal extends JFrame {
         // // Incluímos la JLabel en la colección de componentes de la ventana
         // this.add(label);
         JLabel etiqueta1 = new JLabel("Etiqueta Uno");
-        JLabel etiqueta2 = new JLabel("Etiqueta Dos");
-        JButton boton1 = new JButton("Aceptar");
-        JButton boton2 = new JButton("Cancelar");
-
-        setLayout(null); //Con null TAMAÑO Y POSICIÓN
         etiqueta1.setLocation(0, 0);
+        etiqueta1.setSize(100, 20);
+        // etiqueta1.setBounds(0,0, 10, 20);//lo de arriba simplificado
+        this.add(etiqueta1, BorderLayout.NORTH);
+        
+        JLabel etiqueta2 = new JLabel("Etiqueta Dos");
         etiqueta2.setLocation(100, 0);
-        boton1.setLocation(0, 100);
-        boton2.setLocation(100, 100);
-        etiqueta1.setSize(0, 0);
         etiqueta2.setSize(100, 0);
+        this.add(etiqueta2, BorderLayout.SOUTH);
+        
+        JButton boton1 = new JButton("Aceptar");
+        boton1.setLocation(0, 100);
         boton1.setSize(0, 100);
+        this.add(boton1, BorderLayout.EAST);
+        
+        JButton boton2 = new JButton("Cancelar");
+        boton2.setLocation(100, 100);
         boton2.setSize(100, 100);
+        this.add(boton2, BorderLayout.WEST);
+        
+        setLayout(null); //Con null TAMAÑO Y POSICIÓN
         
         //1º POSICIÓN Y 2º TAMAÑO
         etiqueta1.setBounds(100,100,100,100);
-
-
+        
+        
         setLayout(new FlowLayout());//CON FLOW SOLO POSICIÓN
         etiqueta1.setLocation(0, 0);
         etiqueta2.setLocation(100, 0);
@@ -51,9 +65,5 @@ class FrmPrincipal extends JFrame {
         // this.add(boton2);
 
         //.add con distribución para que se vean todos
-        this.add(etiqueta1, BorderLayout.NORTH);
-        this.add(etiqueta2, BorderLayout.SOUTH);
-        this.add(boton1, BorderLayout.EAST);
-        this.add(boton2, BorderLayout.WEST);
     }
 }
