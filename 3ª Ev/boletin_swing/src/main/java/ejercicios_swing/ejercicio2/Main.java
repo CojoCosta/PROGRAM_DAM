2c747c26d78b6b81a73d0360d78eec86786b9fc1
CojoCosta <dcosta1882000@gmail.com>
2025-04-28 12:24:09 +0200

    changes

--- 3ª Ev/boletin_swing/src/main/java/ejercicios_swing/ejercicio2/Main.java
+++ 3ª Ev/boletin_swing/src/main/java/ejercicios_swing/ejercicio2/Main.java
@@ -1,5 +1,16 @@
 package ejercicios_swing.ejercicio2;
 
+import java.awt.FlowLayout;
+
+import javax.swing.JFrame;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        Titulo titulo1 = new Titulo();
+        titulo1.setSize(500, 500);
+        titulo1.setVisible(true);
+        titulo1.setLayout(new FlowLayout());
+        titulo1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
 }
--- 3ª Ev/boletin_swing/src/main/java/ejercicios_swing/ejercicio2/Titulo.java
+++ 3ª Ev/boletin_swing/src/main/java/ejercicios_swing/ejercicio2/Titulo.java
@@ -3,19 +3,51 @@ package ejercicios_swing.ejercicio2;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
 
-public class Titulo extends JFrame implements ActionListener{
-    private JLabel etiqueta;
-    
+public class Titulo extends JFrame implements ActionListener {
+    private JTextField texto;
+    private JButton boton;
+    private JOptionPane preguntaConfirmacion;
+    private JCheckBox alReves;
+    private JCheckBox habilitador;
 
+    public Titulo() {
+        this.setTitle("Tituloo");
 
+        texto = new JTextField(10);
+        this.add(texto);
+        texto.addActionListener(this);
 
+        boton = new JButton("CAMBIAR TEXTO");
+        this.add(boton);
+        boton.addActionListener(this);
+
+        preguntaConfirmacion = new JOptionPane();
+
+        alReves = new JCheckBox("Dar la vuelta");
+        this.add(alReves);
+
+        habilitador = new JCheckBox("Habilita el TextField");
+        this.add(habilitador);
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        
+        if (preguntaConfirmacion.showConfirmDialog(this,
+                String.format("¿Quieres poner el título \"%s\"?", texto.getText())) == JOptionPane.OK_OPTION) {
+            this.setTitle(texto.getText());
+        }
+        if (alReves.isSelected()) {
+            String aux = "";
+            for (int i = texto.getText().length() - 1; i >= 0; i--) {
+                aux += texto.getText().charAt(i);
+            }
+            this.setTitle(aux);
+        }
     }
-    
 }
