72470817c6eaf5a041801ed3fc93492eee8bdef5
CojoCosta <dcosta1882000@gmail.com>
2025-05-09 09:46:11 +0200

    changes

--- 3ª Ev/pruebas_clase/src/main/java/poo_avanzada/GUI_SWING_2/PruebaGUI.java
+++ 3ª Ev/pruebas_clase/src/main/java/poo_avanzada/GUI_SWING_2/PruebaGUI.java
@@ -1,15 +1,31 @@
 package poo_avanzada.GUI_SWING_2;
 
+import java.io.File;
+import java.util.Scanner;
+
 import javax.swing.JFrame;
 
 public class PruebaGUI {
     public static void main(String[] args) {
-        FormularioPrincipal frame = new FormularioPrincipal();
-        frame.setSize(900, 900);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
-        
+        // ComboBox frame = new ComboBox();
+        // frame.setSize(900, 900);
+        // frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // frame.setLocationRelativeTo(null);
+        // frame.setVisible(true);
+
+        Scanner sc = new Scanner(System.in);
+        String path;
+        System.out.printf("Introduce path de archivo o directorio: ");
+        path = sc.nextLine();
+        File f = new File(path);
+        if (f.exists()) {
+            System.out.printf(
+                    "%s existe\n%s un archivo\nTamaño %d bytes\n",
+                    f.getName(), f.isFile() ? "Es" : "No es", f.length());
+            System.out.printf("La ruta completa es %s\n", f.getPath());
+        } else {
+            System.out.println("El fichero/directorio no existe");
+        }
     }
 
 }
